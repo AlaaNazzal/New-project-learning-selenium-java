d730041cde991001207668128cedda2f310d0771
AlaaNazzal <37230385+AlaaNazzal@users.noreply.github.com>
2018-03-12 16:23:53 -0700

    Alaa Selenium first file

---
+++ AlaaMethodes.java
@@ -0,0 +1,38 @@
+package com.learingmethodes;
+
+public class AlaaMethodes {
+
+	public static void main(String[] args) {
+		Introduction();
+	AlaaMethodes n=new AlaaMethodes();
+	n.QAcourse();
+		Payment();
+	}
+
+	public static void Introduction(){
+	 System.out.println("hi, I am alaa, i am writing code for the first time,"
+	 		+ "I am working as a softwere tesing and i am learing Java with my new friend Yoans Zaid,/n"
+	 		+ "java is great tool to learn and i should be able to learn the basic staff to make work the selenuim "
+	 		+ "the course is 2K and i encourgae everon to take the course/n"
+	 		+ "thank you");
+
+	
+	}
+		public void QAcourse(){
+			System.out.println("QA and softwaer testing course");
+			System.out.println("A: end to end testing,"
+					+ "validation and verification testing, intergration testing and ssytem testing as well");
+			
+	}
+		public static void Payment() {
+			System.out.println("$100");
+		}
+
+		public void getText() {
+			// TODO Auto-generated method stub
+			
+		}
+}
+
+		
+		
